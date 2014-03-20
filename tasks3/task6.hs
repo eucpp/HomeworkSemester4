6d63b1272ea976bd8571ca318b4f342ec5553c66
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2014-03-20 11:23:48 +0400

    fix task3_6

--- tasks3/task6.hs
+++ tasks3/task6.hs
@@ -18,20 +18,15 @@ brackets :: [Char] -> [Char] -> Bool
 
 brackets stack [] = stack == []
 
-brackets [] (c:cs)  = if (is_open_bracket c)
-                      then brackets (c:[]) cs
-                      else if (is_close_bracket c)
-                           then False
-                           else brackets [] cs
+brackets [] (c:cs) | is_open_bracket c  = brackets (c:[]) cs
+                   | is_close_bracket c = False
+                   | otherwise          = brackets [] cs
 
-brackets (x:xs) (c:cs) =  if (is_open_bracket c)
-                          then brackets (c:x:xs) cs
-                          else if (is_close_bracket c)
-                               then if (is_pair_brackets x c)
-                                    then brackets xs cs
-                                    else False
-                               else brackets (x:xs) cs
-                              
+brackets (x:xs) (c:cs) | is_open_bracket c       = brackets (c:x:xs) cs
+                       | (is_close_bracket c) &&
+                         (is_pair_brackets x c)  = brackets xs cs
+                       | is_close_bracket c      = False
+                       | otherwise               = brackets (x:xs) cs  
 
 is_correct_brackets :: [Char] -> Bool
 is_correct_brackets = brackets []
