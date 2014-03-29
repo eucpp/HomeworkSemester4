632b40bb36ea47ea60259f80fdc7d12dc3d8b900
Evgeniy Moiseenko <evg.moiseenko@mail.ru>
2014-03-29 21:15:01 +0400

    tasks 5-6 fixed

--- tasks5/task5.hs
+++ tasks5/task5.hs
@@ -1,11 +1,12 @@
 -- Используя монадичесие функции, опишите фунцию, которая ищет в списке первый элемент, больший своих соседей (предыдущего и следующего)
 
+import Control.Monad
+
 greatest_neighboor :: Ord a => [a] -> Maybe a
 greatest_neighboor [] = Nothing
 greatest_neighboor (x:[]) = Nothing
 greatest_neighboor (x:y:[]) = Nothing
-greatest_neighboor (x:y:z:rest) 
-                               | (y > x) && (y > z) = Just y
-                               | otherwise          = greatest_neighboor (y:z:rest)
+greatest_neighboor (x:y:z:rest) = maybeY `mplus` greatest_neighboor (y:z:rest) 
+                                    where maybeY =  if (y > x) && (y > z) then Just y else Nothing 
 
 
